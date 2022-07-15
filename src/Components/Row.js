5756dcf9699d972251e3ec434e5db347bbb1fc92
Abhishek Chandra <abhichandra03@gmail.com>
2022-07-15 16:13:30 +0530

    Second commit

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -27,7 +27,7 @@ const Row = ({ title, fetchURL, rowID }) => {
   return (
     <>
       <div>
-        <h2 className="font-bold md:text-xl p-4">{title}</h2>
+        <h2 className=" text-white font-bold md:text-xl p-4">{title}</h2>
       </div>
 
       <div className="relative flex items-center group">
